2b86d1f3fe1b0e6b52fc348142549dd2d2dd5408
morboneh <mor.boneh@gmail.com>
2026-06-26 12:50:58 +0300

    Extend question model with option to load an exixting question id

--- js/models/Question.js
+++ js/models/Question.js
@@ -1,6 +1,6 @@
 export class Question {
-  constructor(text, answers, correctAnswerIndex) {
-    this.id = crypto.randomUUID();
+  constructor(text, answers, correctAnswerIndex, options = {}) {
+    this.id = options.id ?? crypto.randomUUID();
     this.text = text;
     this.answers = answers;
     this.correctAnswerIndex = correctAnswerIndex;
