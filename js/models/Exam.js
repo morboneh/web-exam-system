378518a2a837f3bfe98c74b91ad1f17b0b9d036b
morboneh <mor.boneh@gmail.com>
2026-06-26 12:42:19 +0300

    Extend exam model

--- js/models/Exam.js
+++ js/models/Exam.js
@@ -1,9 +1,14 @@
 export class Exam {
-  constructor(title) {
-    this.id = crypto.randomUUID();
+  constructor(title, options = {}) {
+    this.id = options.id ?? crypto.randomUUID();
     this.title = title;
-    this.questions = [];
-    this.createdAt = new Date().toISOString();
+    this.description = options.description ?? "";
+    this.category = options.category ?? "";
+    this.code = options.code ?? "";
+    this.duration = options.duration ?? null;
+    this.teacherId = options.teacherId ?? null;
+    this.questions = options.questions ?? [];
+    this.createdAt = options.createdAt ?? new Date().toISOString();
   }
 
   addQuestion(question) {
